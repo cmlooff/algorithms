28fe3c774b88526e3ec7f974d1c4be5ab313fa5b
Christian <106610236+cmlooff@users.noreply.github.com>
2022-08-15 18:40:39 -0400

    sandboxing

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,13 @@
+class Name {
+    constructor (name) {
+        this.name = name;
+    }
 
+    introduce () {
+        console.log('Hello, my name is ${this.name}')
+    }
+}
 
+const person = new Name ('Chris');
+
+console.log(person)
