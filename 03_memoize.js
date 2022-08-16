45ae464c1f89dda7005e8c60ca51a2394e2d8bd0
Christian <106610236+cmlooff@users.noreply.github.com>
2022-08-16 17:55:33 -0400

    memoize function

---
+++ 03_memoize.js
@@ -0,0 +1,6 @@
+/* 
+    Returns a function that when called, will check if it has already been computed
+    the result for the given argument and return that value instead if possible.
+*/
+
+//* Memoize uses closure
